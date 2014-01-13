15c202c08ef7bffd2e9f875afb3d07c900f60afb
Tilo Dietrich <tilo.dietrich@knips-konsorten.de>
2014-01-13 16:39:41 +0100

    Anpassungen E-Mail-Validator

--- lib/validators/email_validator.rb
+++ lib/validators/email_validator.rb
@@ -1,6 +1,8 @@
 class EmailValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
-    unless value =~ /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+    return if options[:allow_blank] && value.blank?
+
+    unless value =~ /\A([\w-\.\+]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
       record.errors[attribute] <<
         (options[:message] || I18n.t("activerecord.errors.messages.not_an_email"))
     end
