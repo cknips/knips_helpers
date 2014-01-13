8d3ffe2317bf2e5af1499a57a19262e1bf8aaeef
Tilo Dietrich <tilo.dietrich@knips-konsorten.de>
2014-01-13 16:54:13 +0100

    Regex in Konstante

--- lib/validators/email_validator.rb
+++ lib/validators/email_validator.rb
@@ -1,8 +1,11 @@
 class EmailValidator < ActiveModel::EachValidator
+  EMAIL_REGEX = /\A([\w-\.\+]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+  
   def validate_each(record, attribute, value)
     return if options[:allow_blank] && value.blank?
 
-    unless value =~ /\A([\w-\.\+]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+
+    unless value =~ EMAIL_REGEX
       record.errors[attribute] <<
         (options[:message] || I18n.t("activerecord.errors.messages.not_an_email"))
     end
