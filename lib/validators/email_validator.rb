7c91ec2e49aacfa7320a9e36df8fed868b53c9b7
Tilo Dietrich <tilo.dietrich@knips-konsorten.de>
2014-01-15 19:05:42 +0100

    Bugfix Regex

--- lib/validators/email_validator.rb
+++ lib/validators/email_validator.rb
@@ -1,5 +1,5 @@
 class EmailValidator < ActiveModel::EachValidator
-  EMAIL_REGEX = /\A([\w-\.\+]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+  EMAIL_REGEX = /\A([\w\-\.\+]+)@((?:[\-a-z0-9]+\.)+[a-z]{2,})\z/i
   
   def validate_each(record, attribute, value)
     return if options[:allow_blank] && value.blank?
