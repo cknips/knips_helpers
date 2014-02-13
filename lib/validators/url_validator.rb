c490150461e72933e6ab51dba3879dbbf8753deb
Tilo Dietrich <tilo.dietrich@knips-konsorten.de>
2014-02-13 17:37:33 +0100

    Besserer Regexp für URL

--- lib/validators/url_validator.rb
+++ lib/validators/url_validator.rb
@@ -1,11 +1,11 @@
 class UrlValidator < ActiveModel::EachValidator
   PORT       = /(([:]\d+)?)/
   DOMAIN     = /([a-z0-9\-]+\.?)*([a-z0-9]{2,})\.[a-z]{2,}/
-  NUMERIC_IP = /(?>(?:1?\d?\d|2[0-4]\d|25[0-5])\.){3}(?:1?\d?\d|2[0-4]\d|25[0-5])(?:\/(?:[12\]?\d|3[012])|-(?> (?:1?\d?\d|2[0-4]\d|25[0-5])\.){3}(?:1?\d?\d|2[0-4]\d|25[0-5]))?/
+  NUMERIC_IP = /\b(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b/
 
   URL_REGEX  = /\A((localhost)|#{DOMAIN}|#{NUMERIC_IP})#{PORT}\z/
   
-  
+
   def validate_each(record, attribute, value)
     return if options[:allow_blank] && value.blank?
 
