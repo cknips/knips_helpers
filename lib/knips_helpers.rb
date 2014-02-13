32e01dc13c26bfa016b495cb7035815af07288d0
Tilo Dietrich <tilo.dietrich@knips-konsorten.de>
2014-02-13 17:14:38 +0100

    Validator für Postleitzahl hinzugefügt

--- lib/knips_helpers.rb
+++ lib/knips_helpers.rb
@@ -3,6 +3,7 @@ require "knips_helpers/engine"
 
 require "validators/email_validator"
 require "validators/url_validator"
+require "validators/zip_validator"
 
 module KnipsHelpers
   # Your code goes here...
---
+++ lib/validators/zip_validator.rb
@@ -0,0 +1,13 @@
+class ZipValidator < ActiveModel::EachValidator
+  ZIP_REGEX = /\A[0-9]{5}\z/
+  
+  def validate_each(record, attribute, value)
+    return if options[:allow_blank] && value.blank?
+
+
+    unless value =~ ZIP_REGEX
+      record.errors[attribute] <<
+        (options[:message] || I18n.t("activerecord.errors.messages.not_a_zip"))
+    end
+  end
+end
