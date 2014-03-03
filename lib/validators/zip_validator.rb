19c0e7437f8fb479946a8accc91cd3907462f49d
Tilo Dietrich <tilo.dietrich@knips-konsorten.de>
2014-03-03 12:06:48 +0100

    URL-Validator verbessert

--- lib/validators/zip_validator.rb
+++ lib/validators/zip_validator.rb
@@ -1,6 +1,6 @@
 class ZipValidator < ActiveModel::EachValidator
   ZIP_REGEX = /\A[0-9]{5}\z/
-  
+
   def validate_each(record, attribute, value)
     return if options[:allow_blank] && value.blank?
 
