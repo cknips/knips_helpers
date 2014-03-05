3806ca2ce3519f90e83d0582fe3a2a39a35a5cce
Tilo Dietrich <tilo.dietrich@knips-konsorten.de>
2014-03-05 11:43:07 +0100

    URL-Validator repariert (schon wieder...)

--- lib/validators/url_validator.rb
+++ lib/validators/url_validator.rb
@@ -1,16 +1,14 @@
 class UrlValidator < ActiveModel::EachValidator
-  PROTOCOL   = /(?:(?:http|https)\:\/\/)?/
-  DOMAIN     = /([a-z0-9\-]+\.?)*([a-z0-9]{2,})\.[a-z]{2,}/
-  NUMERIC_IP = /\b(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b/
-  PORT       = /(([:]\d+)?)/
-  PATH       = /\/?[a-zA-Z0-9\-\._\?\,\'\/\\\+&%\$#\=~]/
+  PROTOCOL       = /(?:(?:http|https)\:\/\/)?/
+  DOMAIN         = /([a-z0-9\-]+\.?)*([a-z0-9]{2,})\.[a-z]{2,}/
+  PORT           = /(([:]\d+)?)/
+  PATH_AND_QUERY = /\/?[a-zA-Z0-9\-\._\?\,\'\/\\\+&%\$#\=~]*[^\.\,\s]/
 
-  URL_REGEX  = /\A#{PROTOCOL}((localhost)|#{DOMAIN}|#{NUMERIC_IP})#{PORT}#{PATH}\z/
+  URL_REGEX      = /\A#{PROTOCOL}#{DOMAIN}#{PORT}#{PATH_AND_QUERY}\z/
 
 
   def validate_each(record, attribute, value)
     return if options[:allow_blank] && value.blank?
-
 
     unless value =~ URL_REGEX
       record.errors[attribute] <<
