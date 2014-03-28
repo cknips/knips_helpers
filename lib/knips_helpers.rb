1dc8eaa15976196e0cc685ff80f835e53803a155
Alexander Huber <alexander.huber@knips-konsorten.de>
2014-03-28 14:00:31 +0100

    IBAN-Validator hinzugefügt

--- lib/knips_helpers.rb
+++ lib/knips_helpers.rb
@@ -4,6 +4,7 @@ require "knips_helpers/engine"
 require "validators/email_validator"
 require "validators/url_validator"
 require "validators/zip_validator"
+require "validators/iban_validator"
 
 module KnipsHelpers
   # Your code goes here...
---
+++ lib/validators/iban_validator.rb
@@ -0,0 +1,13 @@
+class IbanValidator < ActiveModel::EachValidator
+  def validate_each(record, attribute, value)
+    return if options[:allow_blank] && value.blank?
+
+    value.gsub!(/\s+/, "")
+    iban = value.gsub(/[A-Z]/) { |p| p.ord - 55 }
+    unless (iban[6..iban.length-1].to_s + iban[0..5].to_s).to_i % 97 == 1
+      record.errors[attribute] <<
+        (options[:message] || I18n.t("activerecord.errors.messages.not_an_iban"))
+    end
+  end
+end
+
