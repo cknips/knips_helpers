e03183546a282503f6be68a618abe8862c22a25e
Tilo Dietrich <tilo.dietrich@knips-konsorten.de>
2014-07-14 16:25:52 +0200

    E-Mail-Validator: Umlaute auch in TLD erlaubt

--- lib/validators/email_validator.rb
+++ lib/validators/email_validator.rb
@@ -1,5 +1,5 @@
 class EmailValidator < ActiveModel::EachValidator
-  EMAIL_REGEX = /\A([\wÄÖÜäöüß\-\.\+]+)@((?:[\-a-z0-9äöüß]+\.)+[a-z]{2,})\z/i
+  EMAIL_REGEX = /\A([\wÄÖÜäöüß\-\.\+]+)@((?:[\-a-z0-9äöüß]+\.)+[a-zäöüß]{2,})\z/i
 
   def validate_each(record, attribute, value)
     return if options[:allow_blank] && value.blank?
