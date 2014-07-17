c06f65483b7de71511e57aa16044f10668f5d9ad
Alexander Huber <alexander.huber@knips-konsorten.de>
2014-07-17 12:58:41 +0200

    IBAN-Validator validiert auf 22 Zeichen

--- lib/validators/iban_validator.rb
+++ lib/validators/iban_validator.rb
@@ -3,6 +3,11 @@ class IbanValidator < ActiveModel::EachValidator
     return if value == nil
     return if options[:allow_blank] && value.blank?
 
+    if value.length != 22
+      record.errors[attribute] <<
+        (options[:message] || I18n.t("activerecord.errors.messages.not_an_iban"))
+    end
+
     value.gsub!(/\s+/, "")
     iban = value.gsub(/[A-Z]/) { |p| p.ord - 55 }
     unless (iban[6..iban.length-1].to_s + iban[0..5].to_s).to_i % 97 == 1
